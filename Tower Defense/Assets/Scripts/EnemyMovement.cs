d6a70a24936ba61a62925c41355926a6f7613537
Yuki Xiao <kunyu.xiao@scsstudent.org>
2026-06-09 11:58:50 -0700

    feat: Add EnemySpawner

--- Tower Defense/Assets/Scripts/EnemyMovement.cs
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -5,7 +5,7 @@ public class EnemyMovement : MonoBehaviour
     [field: SerializeField]
     public float Speed { get; private set; } = 1f;
     [field: SerializeField]
-    public Waypoint Target { get; private set; }
+    public Waypoint Target { get; set; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +18,7 @@ public class EnemyMovement : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Speed * Time.deltaTime);
         float distance = Vector3.Distance(transform.position, Target.transform.position);
-        if (distance <= Mathf.Epsilon)
+        if (distance < Mathf.Epsilon)
         {
             if (Target.Next == null)
             {
---
+++ Tower Defense/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [field: SerializeField]
+    public EnemyMovement Enemy { get; private set; }
+
+    [field: SerializeField]
+    public Waypoint StartingWaypoint { get; private set; }
+
+    [field: SerializeField]
+    public float Delay { get; private set; } = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating(nameof(Spawn), Delay, Delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Spawn()
+    {
+        EnemyMovement newEnemy = Object.Instantiate(Enemy);
+        newEnemy.Target = StartingWaypoint;
+    }
+}
