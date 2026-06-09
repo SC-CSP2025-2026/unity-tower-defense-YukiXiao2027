96373be170aeae4f157a91513796107b74789343
Yuki Xiao <kunyu.xiao@scsstudent.org>
2026-06-09 12:22:48 -0700

    feat: Enemy Spawner Challenge

--- Tower Defense/Assets/Scripts/EnemySpawner.cs
+++ Tower Defense/Assets/Scripts/EnemySpawner.cs
@@ -11,10 +11,23 @@ public class EnemySpawner : MonoBehaviour
     [field: SerializeField]
     public float Delay { get; private set; } = 5f;
 
+    [field: SerializeField]
+    public float SpawnsRemaining { get; private set; } = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+    }
+
+    void OnEnable()
+    {
         InvokeRepeating(nameof(Spawn), Delay, Delay);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke();
     }
 
     // Update is called once per frame
@@ -26,6 +39,15 @@ public class EnemySpawner : MonoBehaviour
     public void Spawn()
     {
         EnemyMovement newEnemy = Object.Instantiate(Enemy);
+
         newEnemy.Target = StartingWaypoint;
+
+        SpawnsRemaining--;
+        //SpawnsRemaining -= 1; //SpawnsRemaining = SpawnsRemaining - 1;
+
+        if (SpawnsRemaining == 0)
+        {
+            CancelInvoke();
+        }
     }
 }
